4bab3d1a90a2eaeb726c3ddb440f7328fc849137
Paul <pablo_rose34@hotmail.com>
2021-09-29 20:27:51 +0100

    update

--- ex17/pauls_db/pauls_db.c
+++ ex17/pauls_db/pauls_db.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-int main(void)
+char menu_and_choice()
 {
 
 	// print instructions
-	printf("Pauls Database Rebuild\n\n");
-	printf("Enter a for Create database\n");
-	printf("Enter b for Edit database\n");
-	printf("Enter c for Delete database\n");
+        printf("Pauls Database Rebuild\n\n");
+        printf("Enter a for Create database\n");
+        printf("Enter b for Edit database\n");
+        printf("Enter c for Delete database\n\n");
+	printf(">>");
 
-	// get inputs from user
-	char menu1[1];
-	scanf("%s", menu1);
-	printf("test print of user option: %s\n", menu1);
+        // get inputs from user
+        char menu1[0];
+        scanf("%c", menu1);
+	return *menu1;
+}
+
+int main(void)
+{
+	char user_choice = menu_and_choice();
+	printf("The User Chose %c\n", user_choice);
 }
