e7435dca5ed5de92f42ab8e48679d432b063320c
Paul <pablo_rose34@hotmail.com>
2021-09-30 20:11:31 +0100

    update

--- ex17/pauls_db/pauls_db.c
+++ ex17/pauls_db/pauls_db.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void quit_program()
+{
+	printf("Program exited\n\n");
+	exit(1);
+}
+
+char get_database_name()
+{
+
+	printf("Please enter database name(max 10 characters)\n");
+	printf(">>");
+
+        char database_name[9];
+	scanf("%c", database_name);
+	return *database_name;
+
+}
 
 char menu_and_choice()
 {
 
-	// print instructions
         printf("Pauls Database Rebuild\n\n");
         printf("Enter a for Create database\n");
         printf("Enter b for Edit database\n");
-        printf("Enter c for Delete database\n\n");
+        printf("Enter c for Delete database\n");
+	printf("Enter d to quit the program\n\n");
 	printf(">>");
 
-        // get inputs from user
         char menu1[0];
         scanf("%c", menu1);
 	return *menu1;
@@ -19,5 +37,31 @@ char menu_and_choice()
 int main(void)
 {
 	char user_choice = menu_and_choice();
-	printf("The User Chose %c\n", user_choice);
+	char database_name;
+
+	switch (user_choice){
+
+	case 'a':
+		database_name = get_database_name();
+		printf("Database name is %c \n", database_name);
+		break;
+
+	case 'b':
+		printf("Run edit database script\n");
+		break;
+
+	case 'c':
+		printf("Run delete database scripts\n");
+		break;
+
+	case 'd':
+		printf("Run quit program scripts\n");
+		break;
+
+	default:
+		printf("Error, command not recognised, please try again\n");
+		quit_program();
+	}
+
+
 }
